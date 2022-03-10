3a3d37450a3647561207858e5178026d6336d0fa
mehmetterdem <erdemmimet@gmail.com>
2022-03-10 13:08:02 +0300

    enumlar

---
+++ enum/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace enumlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(günler.pazartesi);
+            Console.WriteLine((int)günler.pazartesi);
+
+            int sıcaklık = 32;
+            if (sıcaklık<=(int)havaDurumu.normal)
+            {
+                Console.WriteLine("dısarıya cıkmak ıcın havanın ısınmasını bekleyelim");
+
+            }
+            else if (sıcaklık>=(int)havaDurumu.sıcak)
+            {
+                Console.WriteLine("dısarıya cıkmak ıcın sıcak bir gun");
+            }
+            else if (sıcaklık>(int)havaDurumu.normal&&sıcaklık< (int)havaDurumu.coksıcak)
+            {
+                Console.WriteLine("dısarıya cıkmak için tam zamanı");
+            }
+
+
+
+        }
+    }
+
+    enum günler
+    {
+        pazartesi,
+        salı,
+        carsamba,
+        persembe,
+        cuma,
+        cumartesi,
+        pazar
+
+    }
+    enum havaDurumu
+    {
+        soguk=5,
+        normal=20,
+        sıcak=25,
+        coksıcak=30
+    }
+
+
+}
+
